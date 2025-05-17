cad31431306702452997ec72818477bb32047b1c
Jordan <jwald3@brockport.edu>
2025-05-17 17:35:19 -0400

    updated api route

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -3,7 +3,7 @@ import prisma from '@/lib/prisma';
 
 export async function GET() {
   try {
-    const projects = await prisma.project.findMany({
+    const projects = await prisma.handSpunProject.findMany({
       include: {
         stats: true,
         steps: {
